880fcb7ce258252932dd2d4c95f9f12b39539886
Sander Van Damme <sander.vandamme11@gmail.com>
2023-08-03 11:48:38 +0200

    Added icon-color to tabs blade

--- packages/forms/resources/views/components/tabs.blade.php
+++ packages/forms/resources/views/components/tabs.blade.php
@@ -65,6 +65,7 @@
                 :alpine-active="'tab === \'' . $tabId . '\''"
                 :badge="$tab->getBadge()"
                 :icon="$tab->getIcon()"
+                :icon-color="$tab->getIconColor()"
                 :icon-position="$tab->getIconPosition()"
                 :x-on:click="'tab = \'' . $tabId . '\''"
             >
